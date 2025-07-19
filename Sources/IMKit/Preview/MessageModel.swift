2b45e953fc623611a3768c85b9ac21dd4ebc33c9
zhangcf <vite@88.com>
2025-07-19 18:06:18 +0800

    feat(添加消息类型): ✨

---
+++ Sources/IMKit/Preview/MessageModel.swift
@@ -0,0 +1,76 @@
+//
+//  File.swift
+//  IMKit
+//
+//  Created by 张朝富 on 2025/7/19.
+//
+
+import Foundation
+
+/// MARK:  消息类型
+public enum MessageType:String,Codable,Hashable {
+    case text
+}
+
+/// MARK:  消息状态
+public enum MessageStatus:String,Codable,Hashable {
+    case sending // 发送中
+    case sent // 发送成功
+    case failed // 发送失败
+    case received // 接收成功
+    case read // 已读
+}
+
+/// MARK:  发送者信息
+public struct Sender:Codable,Hashable {
+    public let id:String
+    public let name:String
+    public let avatar:URL
+    
+    public init(id: String, name: String, avatar: URL) {
+        self.id = id
+        self.name = name
+        self.avatar = avatar
+    }
+}
+
+let sender1 = Sender(id: "u_1", name: "张三", avatar: DefaultAvatar2)
+let sender2 = Sender(id: "u_2", name: "李四", avatar: DefaultAvatar3)
+let sender3 = Sender(id: "u_3", name: "王二", avatar: DefaultAvatar4)
+
+/// 聊天消息
+public struct Message:Identifiable,Codable,Hashable {
+    
+    public let id:String
+    public let type:MessageType
+    public var content:String
+    public var createdAt:Date
+    public var sender:Sender
+    public var status:MessageStatus
+    
+    public init(
+        id: String = UUID().uuidString,
+        type: MessageType = .text,
+        content: String,
+        createdAt: Date = .now,
+        sender: Sender,
+        status: MessageStatus = .sent
+    ) {
+        self.id = id
+        self.type = type
+        self.content = content
+        self.createdAt = createdAt
+        self.sender = sender
+        self.status = status
+    }
+    
+}
+
+
+let msg1 = Message(content: "这是一条测试消息", sender:sender1)
+
+let msg2 = Message(content: "这是第二条测试消息", sender:sender2)
+
+let msgLong = Message(content: "这是一条很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长的测试消息", sender:sender2)
+
+
