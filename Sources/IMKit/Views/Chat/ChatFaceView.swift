7def147ea19f5e6a6a80c5b1e16891081f342c81
zhangcf <vite@88.com>
2025-07-21 11:42:10 +0800

    新增表情栏

---
+++ Sources/IMKit/Views/Chat/ChatFaceView.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  IMKit
+//
+//  Created by 张朝富 on 2025/7/21.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
